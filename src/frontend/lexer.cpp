5c0f1b5972ae227b16b07495ca3d51f8ee55968a
zouxianyu <2979121738@qq.com>
2022-10-15 17:45:36 +0800

    加入行号、列号记录功能

--- src/frontend/lexer.cpp
+++ src/frontend/lexer.cpp
@@ -1,6 +1,8 @@
 #include <optional>
 #include <string>
 #include <regex>
+#include <mutex>
+#include <iomanip>
 #include "log.h"
 #include "lexer.h"
 
@@ -27,13 +29,17 @@ inline static std::regex getRegex(const std::string &regex) {
 Pattern::Pattern(std::string pattern, std::function<std::optional<int>(std::string)> callback)
         : regex(getRegex(pattern)), callback(callback) {}
 
+// 记录行号，列号
+static size_t currLine = 1;
+static size_t currCol = 1;
+
 // 包含patterns数组
 // getToken函数通过从上到下遍历patterns数组，匹配第一个匹配的pattern
 // 然后调用其对应的callback，获得token的类型
 #include "lexer_pattern.inc"
 
 std::optional<int> Lexer::getToken() {
-retry:
+    retry:
     // 遍历所有规则，找到第一个匹配的规则
     for (const auto &pattern: patterns) {
         std::smatch match;
@@ -60,6 +66,18 @@ retry:
 int yylex() {
     static std::string s(std::istreambuf_iterator(std::cin), {});
     static Lexer lexer{s};
+
+    // 在开始词法分析时调用一次，打印表头
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [] {
+        log("lexer") <<
+                     std::setw(20) << "token" <<
+                     std::setw(20) << "lexeme" <<
+                     std::setw(10) << "line" <<
+                     std::setw(10) << "column" <<
+                     std::endl;
+    });
+
     if (auto token = lexer.getToken()) {
         return *token;
     }
