6cd8f61809e1012efab164a48235f3cda803e973
zouxianyu <2979121738@qq.com>
2022-10-15 16:15:46 +0800

    支持由参数传入文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <stdio.h>
 #include "log.h"
-#include "lexer.h"
+#include "parser.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     log("main") << "SysY compiler" << std::endl;
+
+    // 将标准输入重定向到给定文件
+    if (argc >= 2) {
+        log("main") << "read from file: " << argv[1] << std::endl;
+        freopen(argv[1], "r", stdin);
+    } else {
+        log("main") << "using standard input" << std::endl;
+    }
 
     yyparse();
 
