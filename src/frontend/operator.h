b90f37aa34409f9d3869595a7e9f6c2d18e8e7fe
zouxianyu <2979121738@qq.com>
2022-10-30 18:12:52 +0800

    加入操作符枚举类型

---
+++ src/frontend/operator.h
@@ -0,0 +1,20 @@
+#ifndef SYSY_COMPILER_OPERATOR_H
+#define SYSY_COMPILER_OPERATOR_H
+
+enum class Operator {
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    AND,
+    OR,
+    NOT,
+    LT,
+    LE,
+    GT,
+    GE,
+    EQ,
+    NE,
+};
+#endif //SYSY_COMPILER_OPERATOR_H
