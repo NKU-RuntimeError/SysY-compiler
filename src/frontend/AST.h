9c49f31d6d2dcf3535f8baf1017fb288a49cd6a4
zouxianyu <2979121738@qq.com>
2022-10-30 18:13:21 +0800

    调整AST

--- src/frontend/AST.h
+++ src/frontend/AST.h
@@ -4,45 +4,69 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <variant>
+#include "operator.h"
 #include "type.h"
 
 // 使用Memory管理内存，最后统一释放，由于bison对智能指针支持不好，因此使用此解决方案
 
 // 使用struct是因为默认权限为public，可以直接访问成员变量、成员函数
 
-// TODO: 增加更多基类，如Expression、Statement，更清晰地表达继承关系
-
 namespace AST {
+
     struct Base {
         virtual ~Base() = default;
     };
-    struct Stmt :Base {
 
-    };
-    struct Expr : Base {
+    struct Stmt : Base {};
+    struct Expr : Base {};
+    struct Decl : Base {};
 
-    };
-    struct ConstExpr : Expr{
+    ////////////////////////////////////////////////////////////////////////////
+    // 编译单元
 
-    };
     struct CompileUnit : Base {
         // 存储：常量、变量声明 或 函数定义
         std::vector<Base *> compileElements;
     };
 
-    struct Decl;
-    struct FunctionDef;
-    struct CompileElement : Base {
-        // type为0说明是decl，为1说明是Funcdef
-        int type;
-        Decl * decl;
-        FunctionDef *functionDef;
-    };
-    // 常量和变量声明的基类
-    struct Decl : Base {
+    ////////////////////////////////////////////////////////////////////////////
+    // 常量、变量初始化
 
+    struct InitializerList;
+    struct InitializerElement {
+        std::variant<Expr *, InitializerList *> element;
     };
-    struct ConstVariableDef;
+
+    struct InitializerList {
+        std::vector<InitializerElement *> elements;
+    };
+
+    // 容器类
+    struct Array {
+        std::string name;
+        std::vector<Expr *> size;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // 常量、变量声明
+
+    struct ConstVariableDef {
+        std::string name;
+        // 数组维度，若普通变量则为空，若为数组则存储数组维度
+        // 注：维度不一定是字面值常量，可以为int a[10/2];
+        std::vector<Expr *> size;
+        // 数组初值，若为普通变量，则仅有0个或1个数值
+        // 若为数组，则以initializer_list的方式存储，如{1, 2, 3, 4}
+        // 注：初始化列表可以嵌套，如{{1, 2}, 3, 4}，此时AST加深一层。也可以不初始化，此时为空指针
+        InitializerElement *initVal;
+    };
+
+    // 容器类
+    struct ConstVariableDefList {
+        std::vector<ConstVariableDef *> constVariableDefs;
+    };
+
     struct ConstVariableDecl : Decl {
         // 声明的常量类型，只存储基本类型，如int，float
         Typename type;
@@ -51,167 +75,126 @@ namespace AST {
         std::vector<ConstVariableDef *> constVariableDefs;
     };
 
-    struct ConstVariableDef : Base {
+    struct VariableDef {
         std::string name;
-        // 数组维度，若普通变量则为空，若为数组则存储数组维度
-        // 注：维度不一定是字面值常量，可以为int a[10/2];
-        std::vector<Base *> size;
-        // 数组初值，若为普通变量，则仅有0个或1个数值
-        // 若为数组，则以initializer_list的方式存储，如{1, 2, 3, 4}
-        // 注：初始化列表可以嵌套，如{{1, 2}, 3, 4}，此时AST加深一层。也可以不初始化，该数组大小为0
-        std::vector<Base *> values;
+        std::vector<Expr *> size;
+        InitializerElement *initVal;
     };
-     // 同ConstVariableDecl和ConstVariableDef
-    struct VariableDef;
+
+    // 容器类
+    struct VariableDefList {
+        std::vector<VariableDef *> variableDefs;
+    };
+
     struct VariableDecl : Decl {
-         Typename type;
-         std::vector<VariableDef *> variableDefs;
-    };
-    struct VariableDef : Base {
-        std::string name;
-        std::vector<Base *> size;
-        std::vector<Base *> values;
+        Typename type;
+        std::vector<VariableDef *> variableDefs;
     };
 
-    // 各类list，只包含一个向量
-    struct List : Base {
-    };
-    struct ConstVarDefList : List{
-        std::vector<ConstVariableDef *> defs;
-    };
-    struct VarDefList : List{
-        std::vector<VariableDef *> defs;
-    };
-    struct ConstVarValueList : List{
-        std::vector<Base *> values;
-    };
-    struct ConstVarElement : Base{
-        // type为0表示是expr，为1表示是list
-        int type;
-        ConstExpr *expr;
-        ConstVarValueList *list;
-    };
-    struct VarValueList : List{
-        std::vector<Base *> values;
-    };
-    struct VarElement : Base{
-        // type为0表示是expr，为1表示是list
-        int type;
-        Expr *expr;
-        VarValueList *list;
-    };
-    struct ParamList : List {
-        std::vector<Expr *>exprs;
-    };
-    // 数组，包括名称和维度
-    struct Array : Base {
-        std::string name;
-        std::vector<Base *> size;
-    };
+    ////////////////////////////////////////////////////////////////////////////
+    // 函数定义
 
-    struct FunctionArgument;
-    struct Block;
-    struct FunctionDef : Base {
-        Typename returnType;
-        std::string name;
-        std::vector<FunctionArgument *> arguments;
-        Block *body;
-    };
-    struct FunctionArgument : Base {
+    // TODO: 函数参数是否继承自Base待定
+    struct FunctionArg {
         Typename type;
         std::string name;
         // 若为数组，则存储数组维度
         // 注：此时第一维为空指针，从第二维存储数值，例：int a[][3]
-        std::vector<Base *> size;
+        std::vector<Expr *> size;
     };
-    struct FunctionArgList : List {
-        std::vector<FunctionArgument *> arguments;
+
+    // 只是一个容器，用于在parser解析过程中临时存储函数参数
+    struct FunctionArgList {
+        std::vector<FunctionArg *> arguments;
     };
 
     struct Block : Base {
         // 存储：常量、变量声明 或 语句
-        std::vector<Base *> blockElements;
+        std::vector<Base *> elements;
     };
-    struct BlockElement : Base {
-        // type为0说明是decl，为1说明是stmt
-        int type;
-        Decl *decl;
-        Stmt *stmt;
+
+    struct FunctionDef : Base {
+        Typename returnType;
+        std::string name;
+        std::vector<FunctionArg *> arguments;
+        Block *body;
     };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // 语句
+
+    struct LValue {
+        std::string name;
+        std::vector<Expr *> size;
+    };
+
     struct AssignStmt : Stmt {
-        // 左值
-        Base *lvalue;
-        // 右值
-        Base *rvalue;
+        LValue *lValue;
+        Expr *rValue;
     };
+
     struct ExprStmt : Stmt {
         Expr *expr;
     };
+
+    struct NullStmt : Stmt {};
+
     struct BlockStmt : Stmt {
-        Block *block;
+        std::vector<Base *> elements;
     };
-    struct LogicalExpr;
+
     struct IfStmt : Stmt {
-        LogicalExpr *condition;
+        Expr *condition;
         Stmt *thenStmt;
         // 若存在else，则存储else语句，否则置为空指针
         Stmt *elseStmt;
     };
     struct WhileStmt : Stmt {
-        Base *condition;
-        Stmt *stmt;
+        Expr *condition;
+        Stmt *body;
     };
-    struct BreakStmt : Stmt {
-    };
-    struct ContiStmt : Stmt {
-    };
+
+    struct BreakStmt : Stmt {};
+
+    struct ContinueStmt : Stmt {};
+
     struct ReturnStmt : Stmt {
         Expr *expr;
     };
-    struct Number;
-    struct PrimaryExpr : Expr {
-        // type为0说明是expr，为1说明是lval，为2说明是number
-        int type;
-        Expr *expr;
-        Array *lval;
-        Number *number;
-    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // 表达式
+
     struct UnaryExpr : Expr {
-        // dectype表示是primaryExpr表达式还是函数返回的Expr
-        int decType;
-        // op为0表示正，为1表示负，为2表示非
-        int op;
-        PrimaryExpr *pExpr;
-        std::string *name;
-        ParamList *paramList;
-    };
-    struct Number : Base {
-        // type为0表示int，为1表示float
-        int type;
-        std::string *str;
-    };
-    struct AsmddExpr : ConstExpr {
-        // 0代表Add，1代表Sub，2代表Mul，3代表Div,4代表Mod
-        int op;
-        Expr *lexpr;
-        Expr *rexpr;
-    };
-    struct RelationExpr : ConstExpr {
-        // 0代表等于，1代表不等于，2代表小于，3代表大于，4代表小于等于，5代表大于等于
-        int op;
-        Expr *lexpr;
-        Expr *rexpr;
-    };
-    struct LogicalExpr : ConstExpr {
-        // 0代表逻辑与，1代表逻辑或
-        int op;
-        Expr *lexpr;
-        Expr *rexpr;
+        Operator op;
+        Expr *expr;
     };
 
+    struct FunctionParamList {
+        std::vector<Expr *> params;
+    };
+
+    struct FunctionCallExpr : Expr {
+        std::string name;
+        std::vector<Expr *> params;
+    };
+
+    struct BinaryExpr : Expr {
+        Operator op;
+        Expr *lhs;
+        Expr *rhs;
+    };
+
+    struct NumberExpr : Expr {
+        Typename type;
+        std::string valueStr;
+    };
+
+    struct VariableExpr : Expr {
+        std::string name;
+        std::vector<Expr *> size;
+    };
 }
-
-// TODO: 实现其他AST
 
 // 根节点
 namespace AST {
