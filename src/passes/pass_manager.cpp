621e64930d1179a7d9f49d093ed2a9d4da45760a
zouxianyu <2979121738@qq.com>
2022-11-07 23:47:44 +0800

    加入TargetMachine

--- src/passes/pass_manager.cpp
+++ src/passes/pass_manager.cpp
@@ -1,4 +1,9 @@
 #include <llvm/Passes/PassBuilder.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Target/TargetMachine.h>
 #include "IR.h"
 #include "hello_world_pass.h"
 #include "pass_manager.h"
@@ -12,7 +17,29 @@ void PassManager::run(llvm::OptimizationLevel level) {
     llvm::CGSCCAnalysisManager CGAM;
     llvm::ModuleAnalysisManager MAM;
 
-    llvm::PassBuilder PB;
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    std::string err;
+    auto triple = "armv7-unknown-linux-gnu";
+    auto target = llvm::TargetRegistry::lookupTarget(triple, err);
+    if (!target) {
+        throw std::logic_error(err);
+    }
+
+    // only for raspberry pi 4b
+    auto CPU = "cortex-a72";
+    auto features = "";
+    auto targetMachine =
+            target->createTargetMachine(triple, CPU, features, {}, {});
+
+    IR::ctx.module.setDataLayout(targetMachine->createDataLayout());
+    IR::ctx.module.setTargetTriple(triple);
+
+    llvm::PassBuilder PB(targetMachine);
 
     PB.registerModuleAnalyses(MAM);
     PB.registerCGSCCAnalyses(CGAM);
