479947ff47ceb98b144c1188899fca742e7b1ac0
zouxianyu <2979121738@qq.com>
2022-11-08 19:06:19 +0800

    在main中使用scope_exit清理内存

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "mem.h"
 #include "IR.h"
 #include "pass_manager.h"
+#include "scope.h"
 
 // 命令行格式：
 // compiler -S -o testcase.s testcase.sy
@@ -32,6 +33,14 @@ int main(int argc, char *argv[]) {
     log("main") << "SysY compiler" << std::endl;
 
     try {
+        // 在try块结束后自动释放内存
+        // 由于使用的是C++17，还没有scope_exit特性，所以用了个非标准的实现
+        nonstd::scope_exit cleanup([] {
+            log("main") << "clean up" << std::endl;
+            Memory::freeAll();
+        });
+
+        // 解析命令行参数
         auto [inputFilename, outputFilename, optLevel] = cmdParse(argc, argv);
 
         // 输入重定向
@@ -40,29 +49,34 @@ int main(int argc, char *argv[]) {
             throw std::runtime_error("failed to open file: " + inputFilename);
         }
 
+        // 生成AST
         yyparse();
 
-        log("main") << "final AST root: " << AST::root << std::endl;
+        log("main") << "AST root at: " << AST::root << std::endl;
 
-        // 打印AST
-        // 在long_code2 testcase中会出现树过大的情况，导致打印出来的AST过长，因此暂时不打印树结构
+        // 打印原始AST
         AST::show();
 
+        // 常量求值，包括：常量初值、全局变量初值、数组维度
         AST::root->constEval(AST::root);
 
+        // 展示常量求值后的AST
         AST::show();
 
+        // IR生成
         AST::root->codeGen();
 
-        // 释放AST占用的内存
+        // 在运行Pass前释放AST占用的内存，降低内存占用峰值
         Memory::freeAll();
 
+        // 展示原始IR
         IR::show();
 
+        // 生成汇编代码
         PassManager::run(optLevel, outputFilename);
 
     } catch (std::exception &e) {
-        log("main") << "error: " << e.what() << std::endl;
+        err("main") << "error: " << e.what() << std::endl;
         return 1;
     }
     return 0;
