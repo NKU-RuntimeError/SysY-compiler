bcd24e76ff3beb63ecb79bfd8af76e81b0d9e306
kyin012 <kylin012@gmail.com>
2022-12-13 19:56:45 +0800

    mem2reg头文件完成

--- src/passes/mem2reg_pass.h
+++ src/passes/mem2reg_pass.h
@@ -1,8 +1,16 @@
-//
-// Created by 57207 on 2022/12/13.
-//
-
 #ifndef SYSY_COMPILER_MEM2REG_PASS_H
 #define SYSY_COMPILER_MEM2REG_PASS_H
+
+#include <llvm/IR/PassManager.h>
+
+namespace llvm {
+
+    class Function;
+
+    class PromotePass : public PassInfoMixin<PromotePass> {
+    public:
+        PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+    };
+}
 
 #endif //SYSY_COMPILER_MEM2REG_PASS_H
